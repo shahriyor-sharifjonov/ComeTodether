fa9ed43d585ee7242234a20c6a79d9b62c539818
Shahriyor <shahriyordeveloping@gmail.com>
2021-08-01 09:51:14 +0300

    initial

--- app/src/scripts/index.ts
+++ app/src/scripts/index.ts
@@ -2,6 +2,8 @@
 const headerButton: HTMLButtonElement =
   document.querySelector(".header__button");
 const headerMenu: HTMLUListElement = document.querySelector(".header__menu");
+const headerLink: HTMLCollection = document.getElementsByClassName("header__link");
+const header2Link: HTMLCollection = document.getElementsByClassName("header2__link");
 let menuOpened = false;
 const menuToggle = () => {
   menuOpened = !menuOpened;
@@ -19,3 +21,35 @@ window.onclick = (e: MouseEvent) => {
   )
     menuToggle();
 };
+
+window.onclick = function(e){
+  if(e.target.classList.contains('header__link')){
+    if(menuOpened){
+      menuToggle()
+      headerLinkCLicked(this)
+      function headerLinkCLicked(e){
+        for(let i = 0; i < headerLink.length; i++){
+          headerLink[i].classList.remove('current');
+        }
+        e.classList.add('current');
+      }
+      
+    }
+  }
+}
+window.onclick = function(e){
+  if(e.target.classList.contains('header2__link')){
+    if(menuOpened){
+      menuToggle()
+      header2LinkCLicked(this)
+      function header2LinkCLicked(e){
+        console.log(e);
+        
+        for(let i = 0; i < header2Link.length; i++){
+          header2Link[i].classList.remove('current');
+        }
+        e.classList.add('current');
+      }
+    }
+  }
+}
